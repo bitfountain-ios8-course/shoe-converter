5ce59713a35f103b0b1bd99ad5ccf432e6f756b7
Gustavo Cunha <gustavocunha@gmail.com>
2014-09-23 16:53:17 -0300

    Create a Shoe Size Converter from US to European.

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var menShoeSizeTextField: UITextField!
+    @IBOutlet weak var convertedSizeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        let sizeFromTextField = menShoeSizeTextField.text
+        let numberFromTextField = sizeFromTextField.toInt()
+        var integerFromTextField = numberFromTextField!
+        let conversionConstant = 30
+        integerFromTextField += 30
+        let stringWithUpdatedShoeSize = "\(integerFromTextField)"
+        convertedSizeLabel.text = stringWithUpdatedShoeSize
+        convertedSizeLabel.hidden = false
+    }
 
 }
 
